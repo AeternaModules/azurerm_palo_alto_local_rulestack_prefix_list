3d31a9bc98702b7cb866a2c11ac7b8bf3e3536ca
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "palo_alto_local_rulestack_prefix_lists" {
-  description = "All palo_alto_local_rulestack_prefix_list resources"
-  value       = azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists
-}
 output "palo_alto_local_rulestack_prefix_lists_audit_comment" {
-  description = "List of audit_comment values across all palo_alto_local_rulestack_prefix_lists"
-  value       = [for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : v.audit_comment]
+  description = "Map of audit_comment values across all palo_alto_local_rulestack_prefix_lists, keyed the same as var.palo_alto_local_rulestack_prefix_lists"
+  value       = { for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : k => v.audit_comment }
 }
 output "palo_alto_local_rulestack_prefix_lists_description" {
-  description = "List of description values across all palo_alto_local_rulestack_prefix_lists"
-  value       = [for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : v.description]
+  description = "Map of description values across all palo_alto_local_rulestack_prefix_lists, keyed the same as var.palo_alto_local_rulestack_prefix_lists"
+  value       = { for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : k => v.description }
 }
 output "palo_alto_local_rulestack_prefix_lists_name" {
-  description = "List of name values across all palo_alto_local_rulestack_prefix_lists"
-  value       = [for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : v.name]
+  description = "Map of name values across all palo_alto_local_rulestack_prefix_lists, keyed the same as var.palo_alto_local_rulestack_prefix_lists"
+  value       = { for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : k => v.name }
 }
 output "palo_alto_local_rulestack_prefix_lists_prefix_list" {
-  description = "List of prefix_list values across all palo_alto_local_rulestack_prefix_lists"
-  value       = [for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : v.prefix_list]
+  description = "Map of prefix_list values across all palo_alto_local_rulestack_prefix_lists, keyed the same as var.palo_alto_local_rulestack_prefix_lists"
+  value       = { for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : k => v.prefix_list }
 }
 output "palo_alto_local_rulestack_prefix_lists_rulestack_id" {
-  description = "List of rulestack_id values across all palo_alto_local_rulestack_prefix_lists"
-  value       = [for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : v.rulestack_id]
+  description = "Map of rulestack_id values across all palo_alto_local_rulestack_prefix_lists, keyed the same as var.palo_alto_local_rulestack_prefix_lists"
+  value       = { for k, v in azurerm_palo_alto_local_rulestack_prefix_list.palo_alto_local_rulestack_prefix_lists : k => v.rulestack_id }
 }
 
